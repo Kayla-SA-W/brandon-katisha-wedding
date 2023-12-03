790778db5fe97625183c65adaeea1306ffc9f357
kayla williams <shalisha.angelique@gmail.com>
2023-12-03 12:42:34 -0500

    Create RSVP section with email

---
+++ src/components/rsvp.tsx
@@ -0,0 +1,137 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import "@fontsource/sacramento";
+import '@fontsource/montserrat';
+import { rsvpResponse, sendConfirmationEmail } from '../modules/email-confirmation';
+
+const Form = styled.div`
+    background-color: #3C1114;
+    width: 70%;
+    height: fit-content;
+    display: flex;
+    padding: 50px;
+    margin: 50px;
+    flex-direction: column;
+`
+
+const Container = styled.div`
+    display: flex;
+    color: white;
+    justify-content: center;
+    align-items: center;
+    font-family: 'montserrat';
+`
+
+const InputBox = styled.div`
+    display: flex;
+    flex-direction: column;
+    > input {
+        width: 200px;
+    };
+    > textarea {
+        width: 200px;
+    }
+`
+
+const Title = styled.div`
+    font-family: 'Sacramento';
+    color: white;
+    font-size: 50px;
+    align-items: center;
+    display: flex;
+    justify-content: center;
+`;
+
+const Inputs = styled.div`
+    display: flex;
+    align-items: left;
+    flex-direction: column;
+    justify-content: center;
+    gap: 20px;
+    margin-right: 15%;
+`
+
+const AcceptDecline = styled.div`
+    display: flex;
+    flex-direction: row;
+    gap: 50px;
+    margin: 20px 0;
+`
+
+const Button = styled.button`
+    margin-top: 10px;
+    width: fit-content;
+    background-color: white;
+    font-family: 'montserrat';
+    border: none;
+    border-radius: 5px;
+`
+
+const RsvpConfirmation = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin-top: 50px;
+`
+
+export const RSVP = () => {
+    const [filledOutForm, setFilledOutForm] = useState(false);
+    const [rsvp, setRsvp] = useState({
+        attending: false,
+        name: '',
+        phone: '',
+        email: '',
+        address: ''
+    });
+
+    const onClick = () => {
+        sendConfirmationEmail(rsvp);
+        rsvpResponse(rsvp);
+        setFilledOutForm(true);
+    }
+    return (
+        <Container id="RSVP">
+            <Form>
+                <Title>
+                    RSVP
+                </Title>
+                {
+                    filledOutForm ? (
+                        <Inputs>
+                <AcceptDecline>
+                        <div>
+                            <input type="radio" id="accept" name="acceptDecline" value='accept' onClick={(e) => setRsvp({...rsvp, attending: true})} />
+                            <label htmlFor="accept">Accepts with Pleasure</label>
+                        </div>
+                        <div>
+                            <input type="radio" id="decline" name="acceptDecline" value='decline' onClick={(e) => setRsvp({...rsvp, attending: false})} />
+                            <label htmlFor="decline">Declines with Regret</label>
+                        </div>
+                    </AcceptDecline>
+                    <InputBox>
+                        <label htmlFor='name'>Name:</label>
+                        <input type="text" id="name" name="name" onChange={(e) => setRsvp({...rsvp, name: e.target.value})}></input>
+                    </InputBox>
+                    <InputBox>
+                        <label htmlFor='phone'>Phone Number:</label>
+                        <input type="text" id="phone" name="phone" onChange={(e) => setRsvp({...rsvp, phone: e.target.value})}></input>
+                    </InputBox>
+                    <InputBox>
+                        <label htmlFor='email'>Email Address:</label>
+                        <input type="text" id="email" name="email" onChange={(e) => setRsvp({...rsvp, email: e.target.value})}></input>
+                    </InputBox>
+                    <InputBox>
+                        <label htmlFor='mailing'>Mailing Address:</label>
+                        <textarea id="mailing" name="mailing" rows={5} onChange={(e) => setRsvp({...rsvp, address: e.target.value})}></textarea>
+                    </InputBox>
+                    <Button onClick={onClick}>Submit</Button>
+                </Inputs>
+                    ) : (
+                        <RsvpConfirmation>Thank you for your RSVP.</RsvpConfirmation>
+                    )
+                }
+            </Form>
+        </Container>
+    )
+}
---
+++ src/modules/email-confirmation.tsx
@@ -0,0 +1,31 @@
+import emailjs from 'emailjs-com';
+
+export interface Rsvp {
+    name: string;
+    attending: boolean;
+    phone: string;
+    email: string;
+    address: string;
+}
+
+const attendingMessage = "We are so thankful that you will be able to join us on our special day! We are looking forward to celebrating with you.";
+const notAttendingMessage = "We are so sorry that you will not be able to join us on our special day. We will miss you and hope to see you soon!";
+
+export const sendConfirmationEmail = (rsvp: Rsvp) => {
+    emailjs.send("service_4oqeqrs", "template_mllsm37", {
+        name: rsvp.name,
+        attending: rsvp.attending ? 'Will be attending' : 'Will not be attending',
+        phone: rsvp.phone,
+        email: rsvp.email,
+        address: rsvp.address
+    }, 'user_nY97bkcJucuzmbFNnsTFY');
+};
+
+export const rsvpResponse = (rsvp: Rsvp) => {
+    emailjs.send("service_4oqeqrs", "template_nid8ypm", {
+        name: rsvp.name,
+        attending: rsvp.attending ? 'yes' : 'no',
+        message: rsvp.attending ? attendingMessage : notAttendingMessage,
+        email: rsvp.email
+    }, 'user_nY97bkcJucuzmbFNnsTFY');
+};

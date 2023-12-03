4068014fcf8d0e094e6b9158c5272ae2ef24e2eb
kayla williams <shalisha.angelique@gmail.com>
2023-12-03 14:52:18 -0500

    update gatsby config

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,27 +1,26 @@
-import type { GatsbyConfig } from "gatsby";
-
-const config: GatsbyConfig = {
+module.exports = {
   siteMetadata: {
-    title: `brandon-katisha-wedding`
+    title: `brandon-katisha-wedding`,
+    siteUrl: `https://www.yourdomain.tld`
   },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
-  graphqlTypegen: true,
-  plugins: [{
-    resolve: `gatsby-source-filesystem`,
-    options: {
-      name: `images`,
-      path: `${__dirname}/src/images`
+  plugins: [
+    {
+        resolve: `gatsby-source-filesystem`,
+        options: {
+          name: `images`,
+          path: `${__dirname}/src/images`
+        },
     },
-  },
-  {
-    resolve: 'gatsby-plugin-tslint',
-    options: {
-      test: /\.ts$|\.tsx$/,
-      exclude: /(node_modules|cache|public)/
-    }
-  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-styled-components"]
+    {
+      resolve: 'gatsby-plugin-tslint',
+      options: {
+        test: /\.ts$|\.tsx$/,
+        exclude: /(node_modules|cache|public)/
+      }
+    },
+      `gatsby-plugin-image`,
+      `gatsby-transformer-sharp`, 
+      `gatsby-plugin-sharp`,
+      `gatsby-plugin-styled-components`
+]
 };
-
-export default config;

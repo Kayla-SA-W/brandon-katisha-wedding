3c35f29beabb6d4c182afcff7d2d1e2cdefac2ff
kayla williams <shalisha.angelique@gmail.com>
2023-12-03 12:36:57 -0500

    Initial Set Up

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,17 +2,24 @@ import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `brandon-katisha-wedding`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: `brandon-katisha-wedding`
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
   plugins: [{
-    resolve: 'gatsby-source-wordpress',
+    resolve: `gatsby-source-filesystem`,
     options: {
-      "url": ""
+      name: `images`,
+      path: `${__dirname}/src/images`
+    },
+  },
+  {
+    resolve: 'gatsby-plugin-tslint',
+    options: {
+      test: /\.ts$|\.tsx$/,
+      exclude: /(node_modules|cache|public)/
     }
   }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-styled-components", {
     resolve: 'gatsby-source-filesystem',

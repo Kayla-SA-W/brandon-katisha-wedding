abf826e61b7be568dcc945ae594b96fe8872d2dc
kayla williams <shalisha.angelique@gmail.com>
2024-01-22 22:19:28 -0500

    Add FAQ data

---
+++ src/data/faq.ts
@@ -0,0 +1,38 @@
+export const faq = [
+    {
+        question: 'Is there  a dress code?',
+        answer: 'Formal or African attire'
+    }, 
+    {
+        question: 'Can I bring a plus one?',
+        answer: "While we would love to include everyone's plus ones, space is limited. We hope you understand, and we can't wait to celebrate with you!"
+    },
+    {
+        question: 'Are children welcome?',
+        answer: "While we adore children, we've decided to host an adults-only celebration. We hope you understand and can make arrangements for a night out. We appreciate your support!"
+    },
+    {
+        question: 'Is there parking available near your wedding venue(s)?',
+        answer: "Free parking is available at the wedding venue. There will also be parking attendants at the venue that will assist with parking."
+    },
+    {
+        question: 'Have you reserved blocks of rooms at one or more hotels?',
+        answer: "Yes. Click the tab “Accommodations” to see hotel choices."
+    },
+    {
+        question: 'Will there be an open bar?',
+        answer: "The cocktail hour will have an open bar. During the reception, our celebration will have a cash bar, so guests can purchase their preferred drinks. We can't wait to raise a toast with you!"
+    },
+    {
+        question: 'Am I allowed to take photos at your wedding?',
+        answer: "For the ceremony, we would love to have an unplugged atmosphere where guests can fully experience the moment without distractions. You're welcome to take photos during the reception."
+    },
+    {
+        question: 'How do I RSVP?',
+        answer: "Formal invitations have not been sent. Approximately 3 months before the weddings invites will be sent and that's where you will get your opportunity to RSVP."
+    },
+    {
+        question: 'Do you have a wedding hashtag?',
+        answer: 'Of course we do! #ForeverPhillips'
+    }
+];

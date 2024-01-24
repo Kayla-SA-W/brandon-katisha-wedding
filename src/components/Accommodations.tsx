742fb103592210832bd1ef31bf9ec64698eee24c
kayla williams <shalisha.angelique@gmail.com>
2024-01-23 22:28:53 -0500

    Update hotel link

--- src/components/Accommodations.tsx
+++ src/components/Accommodations.tsx
@@ -62,99 +62,98 @@ export const Accommodations = () => (
     <Hotels id="Accommodations">
         <GlobalStyle />
         <Container>
-        <Title> Accommodations </Title>
+            <Title> Accommodations </Title>
             {/* Hotel 1 */}
-        <HotelBlock>
+            <HotelBlock>
                 <HotelImage src={Hotel} />
                 <Heading>
-                Courtyard by Marriott
+                    Courtyard by Marriott
                 </Heading>
                 <Paragraph>
-                300 River Ridge Drive
-                Norwood, MA 02062
+                    300 River Ridge Drive
+                    Norwood, MA 02062
                 </Paragraph>
-            <Paragraph >
+                <Paragraph >
                     6.2 miles away from the venue
-            </Paragraph>
-            <Link to='https://www.marriott.com/event-reservations/reservation-link.mi?id=1704856395514&key=GRP&app=resvlink' target="_blank"> Click here to book a room under our hotel block</Link>
-            <br />
-            <Paragraph>Price: $220 - $240/night</Paragraph>
-        </HotelBlock>
+                </Paragraph>
+                <Link to='https://www.marriott.com/event-reservations/reservation-link.mi?id=1704856395514&key=GRP&app=resvlink' target="_blank"> Click here to book a room under our hotel block</Link>
+                <br />
+                <Paragraph>Price: $220 - $240/night</Paragraph>
+            </HotelBlock>
 
-        {/* Hotel 2 */}
-        <HotelBlock>
+            {/* Hotel 2 */}
+            <HotelBlock>
                 <HotelImage src={Hotel} />
                 <Heading>
                     EnVision Hotel & Conference Center Mansfield-Foxboro
                 </Heading>
-            <Paragraph>
-                31 Hampshire Street
-                Mansfield, MA 02048
-            </Paragraph>
-            <Paragraph >
+                <Paragraph>
+                    31 Hampshire Street
+                    Mansfield, MA 02048
+                </Paragraph>
+                <Paragraph >
                     9.5 miles away from the venue
-            </Paragraph>
-            <Link to='https://secure.guestcentric.net/api/bg/book.php?nrNights=1&startDay=2024-07-24&preselect=promo294&apikey=7d509f9196c9802e1b08ee9245092c5e&pc=BellegardeWedding' target="_blank"> Click here to book a room under our hotel block</Link>
-            <br />
-            <Paragraph>Price: $249/night </Paragraph>
-        </HotelBlock>
+                </Paragraph>
+                <Link to='https://secure.guestcentric.net/api/bg/book.php?nrNights=1&startDay=2024-07-24&preselect=promo294&apikey=7d509f9196c9802e1b08ee9245092c5e&pc=BellegardeWedding' target="_blank"> Click here to book a room under our hotel block</Link>
+                <br />
+                <Paragraph>Price: $249/night </Paragraph>
+            </HotelBlock>
 
-        {/* Hotel 3 */}
-        <HotelBlock>
+            {/* Hotel 3 */}
+            <HotelBlock>
                 <HotelImage src={Hotel} />
                 <Heading>
-                Fairfield Inn & Suites by Marriott Boston Walpole
+                    Fairfield Inn & Suites by Marriott Boston Walpole
                 </Heading>
-            <Paragraph>
-                630 Providence Hwy
-                Walpole, MA 02081
-            </Paragraph>
-            <Paragraph style={{fontWeight: 500}}>
+                <Paragraph>
+                    630 Providence Hwy
+                    Walpole, MA 02081
+                </Paragraph>
+                <Paragraph style={{ fontWeight: 500 }}>
                     3.7 miles away from the venue
-            </Paragraph>
-            <Paragraph>
-                    We do NOT have a hotel block here.
-            </Paragraph>
-            <Link to='https://www.marriott.com/en-us/hotels/bospo-fairfield-inn-and-suites-boston-walpole/overview/' target="_blank"> Click here to book a room</Link>
-        </HotelBlock>
+                </Paragraph>
+                <Link to="https://www.marriott.com/events/start.mi?id=1706033688368&key=GRP">Click here to book a room under our hotel block</Link>
+                <br />
+                <Paragraph>Price: $239/night </Paragraph>
+            </HotelBlock>
 
-        {/* Hotel 4 */}
-        <HotelBlock>
+            {/* Hotel 4 */}
+            <HotelBlock>
                 <HotelImage src={Hotel} />
                 <Heading>
                     Home2 Suites by Hilton Walpole Foxboro
                 </Heading>
-            <Paragraph>
-                2375 Boston Providence Hwy
-                Walpole, MA 02081
-            </Paragraph>
-            <Paragraph >
+                <Paragraph>
+                    2375 Boston Providence Hwy
+                    Walpole, MA 02081
+                </Paragraph>
+                <Paragraph >
                     4.4 miles away from the venue
-            </Paragraph>
-            <Paragraph>
+                </Paragraph>
+                <Paragraph>
                     We do NOT have a hotel block here.
-            </Paragraph>
-            <Link to='https://www.hilton.com/en/hotels/boswkht-home2-suites-walpole-foxboro/?SEO_id=GMB-AMER-HT-BOSWKHT&y_source=1_OTQwMzQzOS03MTUtbG9jYXRpb24ud2Vic2l0ZQ%3D%3D' target="_blank"> Click here to book a room</Link>
-        </HotelBlock>
+                </Paragraph>
+                <Link to='https://www.hilton.com/en/hotels/boswkht-home2-suites-walpole-foxboro/?SEO_id=GMB-AMER-HT-BOSWKHT&y_source=1_OTQwMzQzOS03MTUtbG9jYXRpb24ud2Vic2l0ZQ%3D%3D' target="_blank"> Click here to book a room</Link>
+            </HotelBlock>
 
-        {/* Hotel 5 */}
-        <HotelBlock>
+            {/* Hotel 5 */}
+            <HotelBlock>
                 <HotelImage src={Hotel} />
                 <Heading>
                     Hilton Garden Inn Foxborough Patriot Place
                 </Heading>
-            <Paragraph>
-                27 Patriot Place
-                Foxborough, MA 02035
-            </Paragraph>
-            <Paragraph >
+                <Paragraph>
+                    27 Patriot Place
+                    Foxborough, MA 02035
+                </Paragraph>
+                <Paragraph >
                     4.5 miles away from the venue
-            </Paragraph>
-            <Paragraph>
+                </Paragraph>
+                <Paragraph>
                     We do NOT have a hotel block here.
-            </Paragraph>
-            <Link to='https://www.hilton.com/en/hotels/bosppgi-hilton-garden-inn-foxborough-patriot-place/?SEO_id=GMB-AMER-GI-BOSPPGI&y_source=1_NDQ2NjA1NC03MTUtbG9jYXRpb24ud2Vic2l0ZQ%3D%3D' target="_blank"> Click here to book a room</Link>
-        </HotelBlock>
+                </Paragraph>
+                <Link to='https://www.hilton.com/en/hotels/bosppgi-hilton-garden-inn-foxborough-patriot-place/?SEO_id=GMB-AMER-GI-BOSPPGI&y_source=1_NDQ2NjA1NC03MTUtbG9jYXRpb24ud2Vic2l0ZQ%3D%3D' target="_blank"> Click here to book a room</Link>
+            </HotelBlock>
         </Container>
     </Hotels>
 ); 

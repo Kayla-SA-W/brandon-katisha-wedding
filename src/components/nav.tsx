713145015a6cabf81cdd626cb9508da6299c8ae8
kayla williams <shalisha.angelique@gmail.com>
2024-02-02 23:05:26 -0500

    Add z index to nav

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Container = styled.div`
+    z-index: 1;
     position: fixed;
     width: 100vw;
     background-color: white;
